0db2780f3c0d50000fa7bdbdd84e42774447f567
Wyatt Howe <wyatt@wyatthowe.net>
2019-11-25 00:36:32 -0500

    scrap work (only as a backup)

---
+++ ecc.js
@@ -0,0 +1,78 @@
+const sodium = require('libsodium-wrappers');
+
+function encrypt(m) {
+  return m;
+}
+
+function decrypt(c) {
+  return c;
+}
+
+function add(a, b) {
+  let c = new Uint8Array(16);
+  for (var i = 0; i < 16; i++) {
+    c[i] = a[i] + b[i];
+  }
+  return c;
+}
+
+function sub(a, b) {
+  let c = new Uint8Array(16);
+  for (var i = 0; i < 16; i++) {
+    c[i] = a[i] - b[i];
+  }
+  return c;
+}
+
+sodium.ready.then(function () {
+  let m0 = new Uint8Array(16).fill(0);
+  let m1 = new Uint8Array(16).fill(1);
+  let x0 = sodium.randombytes_buf(16);
+  let x1 = sodium.randombytes_buf(16);
+  // send x0, x1
+
+  let c = 1;
+  let k = sodium.randombytes_buf(16);
+  let v = add(c ? x1 : x0, encrypt(k));
+  // send v
+
+  let m0k = add(m0, decrypt(sub(v, x0)));
+  let m1k = add(m1, decrypt(sub(v, x1)));
+  // send m0k, m1k
+
+  let mc = sub(c ? m1k : m0k, k);
+
+  console.log(m0, m1, c, mc);
+});
+
+
+
+
+// sodium.ready.then(function () {
+//   // console.log(sodium.crypto_secretbox_easy(
+//   //   new Uint8Array(24).fill(0),
+//   //   new Uint8Array(24).fill(0),  // nonce 24 bytes becasue this sodium uses 192 bit blocks
+//   //   sodium.from_hex('da5698be17b9b46962335799779fbeca8ce5d491c0d26243bafef9ea1837a9d8')
+//   // ));
+//   //
+//   // console.log(sodium.crypto_box_easy(
+//   //   new Uint8Array(8).fill(0),
+//   //   new Uint8Array(24).fill(0),
+//   //   new Uint8Array(32).fill(0),
+//   //   new Uint8Array(32).fill(0)
+//   // ));
+//
+//
+//   var key = sodium.crypto_box_keypair();
+//   var plaintext = new Uint8Array(16);
+//   var message_id = 0;
+//   var nonce = new Uint8Array(24).fill(message_id);
+//
+//   var encrypted = sodium.crypto_box_easy(plaintext, nonce, key.publicKey, key.privateKey);
+//   console.log(encrypted);
+//   encrypted[7] = 0;
+//   console.log(encrypted);
+//
+//   var decrypted = sodium.crypto_box_open_easy(encrypted, nonce, key.publicKey, key.privateKey);
+//   console.log(decrypted);
+// });
--- src/lib/ot.js
+++ src/lib/ot.js
@@ -11,46 +11,25 @@ const init = function(socket) {
   */
   const send = function(a, b) {
     var msg_id = socket.nextid();
-    socket.call('oblv', {msg_id: msg_id, length: bytes + 1});
 
     return new Promise(function (resolve) {
-      socket.hear('oblv' + msg_id).then(function (r0_r1_JSON) {
-        const r0_r1 = JSON.parse(r0_r1_JSON);
-        const r0 = r0_r1[0];
-        const r1 = r0_r1[1];
-
-        socket.get('e' + msg_id).then(function (e_JSON) {
-          const e = parseInt(e_JSON);
-          const f0 = crypto.xor_array(a, e ? r1 : r0);
-          const f1 = crypto.xor_array(b, e ? r0 : r1);
-
-          socket.give('f' + msg_id, '[' + f0.stringify() + ',' + f1.stringify() + ']');
-          resolve();
-        });
-      });
+      socket.give(msg_id, '[' + a.stringify() + ',' + b.stringify() + ']');
+      resolve();
     });
   }
 
   const receive = function(c) {
     var msg_id = socket.nextid();
-    socket.call('oblv', {msg_id: msg_id, length: bytes + 1});
 
     return new Promise(function (resolve) {
-      socket.hear('oblv' + msg_id).then(function (d_rd_JSON) {
-        const d_rd = JSON.parse(d_rd_JSON);
-        const d = d_rd[0];
-        const r_d = d_rd[1];
+      socket.get(msg_id).then(function (f_JSON) {
+        const f = JSON.parse(f_JSON);
+        const f0 = f[0];
+        const f1 = f[1];
 
-        socket.give('e' + msg_id, c ^ d);
-        socket.get('f' + msg_id).then(function (f_JSON) {
-          const f = JSON.parse(f_JSON);
-          const f0 = f[0];
-          const f1 = f[1];
-
-          const m_c = crypto.xor_array(r_d, c ? f1 : f0);
-          const label = Label(m_c);
-          resolve(label);
-        });
+        const m_c = c ? f1 : f0;
+        const label = Label(m_c);
+        resolve(label);
       });
     });
   }
