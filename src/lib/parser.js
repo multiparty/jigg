87097e0a0661b24b6d7ab2a95df9e655279f88c0
Andrei Lapets <a@lapets.io>
2019-12-22 16:28:07 -0500

    Refactor Bristol parser into its own function.

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -5,49 +5,49 @@ const socket = require('./socket.js');
 const bytes = 16;
 const types = {'AND': 'and', 'XOR': 'xor', 'INV': 'not'};
 
-function circuit_load_bristol(path, port) {
+function circuit_parse_bristol(raw) {
   var circuit = {
     wires: 0, gates: 0,
     input: [], output: [],
     gate: []
   };
 
+  const rows = raw.split('\n').map(function (ln) { return ln.split(' '); });
+  circuit.gates = +rows[0][0];
+  circuit.wires = +rows[0][1];
+
+  if (rows[1][0] != 2) {
+    // Asymmetric inputs.
+  }
+
+  for (var i = 1; i <= rows[1][0] * rows[1][1]; i++) {
+    circuit.input.push(i);
+  }
+
+  for (var i = 1+circuit.wires-(rows[2][0]*rows[2][1]); i <= circuit.wires; i++) {
+    circuit.output.push(i);
+  }
+
+  // Parse the individual gates.
+  for (var row = 3; row < circuit.gates+3; row++) {
+    var tokens = rows[row];
+    var gate = {wirein: [], wireout: undefined, type: 'unknown'};
+    gate.wirein = [1 + (+tokens[2])];
+    if (parseInt(tokens[0]) === 2) {
+      gate.wirein.push(1 + (+tokens[3]));
+    }
+    gate.wireout = 1 + (+tokens[2 + (+tokens[0])]);
+    gate.type = types[tokens[3 + (+tokens[0])]];
+    circuit.gate.push(gate);
+  }
+
+  return circuit;
+}
+
+function circuit_load_bristol(path, port) {
   return new Promise(function (resolve) {
-    socket.geturl(path, 'text', port).then(function (text) {
-      const bristol = text.split('\n').map(function (line) {
-        return line.split(' ');
-      });
-
-      circuit.gates = +bristol[0][0];
-      circuit.wires = +bristol[0][1];
-
-      if (bristol[1][0] != 2) {
-        // Asymmetric inputs!!
-      }
-
-      for (var i = 1; i <= bristol[1][0] * bristol[1][1]; i++) {
-        circuit.input.push(i);
-      }
-
-      for (i = 1+circuit.wires-(bristol[2][0]*bristol[2][1]); i <= circuit.wires; i++) {
-        circuit.output.push(i);
-      }
-
-      for (i = 0; i < circuit.gates; i++) {
-        var args = bristol[i+3];
-
-        var gate = {};
-        gate.wirein = [1+(+args[2])];
-        if (parseInt(args[0]) === 2) {
-          gate.wirein.push(1+(+args[3]));
-        }
-        gate.wireout = 1+(+args[2+(+args[0])]);
-        gate.type = types[args[3+(+args[0])]];
-
-        circuit.gate.push(gate);
-      }
-
-      resolve(circuit);
+    socket.geturl(path, 'text', port).then(function (txt) {
+      resolve(circuit_parse_bristol(txt));
     });
   });
 }
