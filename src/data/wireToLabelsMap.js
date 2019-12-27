8ce6639a8f53b59c742dd27a88844dc0a9edfcab
Andrei Lapets <a@lapets.io>
2019-12-26 23:00:05 -0500

    Add wire-to-labels data structure methods.

--- src/data/wireToLabelsMap.js
+++ src/data/wireToLabelsMap.js
@@ -7,6 +7,7 @@
 
 const gate = require('./gate.js');
 const circuit = require('./circuit.js');
+const label = require('./label.js');
 
 /**
  * Create a new wire-to-labels map data structure instance.
@@ -26,11 +27,41 @@ WireToLabelsMap.prototype.set = function (index, labels) {
 };
 
 /**
+ * Get the labels at the specified wire index.
+ * @param {number} index - Index of wire for which to return the label
+ * @param {Object[]} labels - Array of one or two labels
+ */
+WireToLabelsMap.prototype.get = function (index) {
+  return this.mapping[index];
+};
+
+/**
  * Return the data structure instance as a JSON object.
- * @returns {Object} The data structure as a JSON object
+ * @returns {Object} Data structure as a JSON object
  */
 WireToLabelsMap.prototype.toJSON = function () {
-  return this.mapping;
+  var json = {};
+  for (var index in this.mapping) {
+    if (this.mapping[index].isArray == null) {
+      json[index] = this.mapping[index].toJSON();
+    } else {
+      json[index] = this.mapping[index];
+    }
+  }
+  return json;
+};
+
+/**
+ * Return a subset of the map corresponding to the supplied indices.
+ * @param {number[]} indices - Indices of map entries to keep in result
+ * @returns {Object} Data structure as a JSON object
+ */
+WireToLabelsMap.prototype.copyWithOnlyIndices = function (indices) {
+  var wireToLabels = new WireToLabelsMap();
+  for (var k = 0; k < indices.length; k++) {
+    wireToLabels.set(indices[k], this.mapping[indices[k]]);
+  }
+  return wireToLabels;
 };
 
 /**
@@ -39,11 +70,12 @@ WireToLabelsMap.prototype.toJSON = function () {
  * @returns {Object} Mapping from each wire index to a list of labels
  */
 function initializeWireToLabelsMap(circuit) {
-  var wiresToLabels = [null];
-  for (var i = 0; i < circuit.wires; i++) {
-    wiresToLabels.push([]);
+  var wireToLabels = new WireToLabelsMap();
+  wireToLabels.set(0, null);
+  for (var i = 1; i < circuit.wires+1; i++) {
+    wireToLabels.set(i, []);
   }
-  return wiresToLabels;
+  return wireToLabels;
 }
 
 module.exports = {
