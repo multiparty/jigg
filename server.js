e6349df15766d17b33d8b69ca25f2e977c82c2dc
Andrei Lapets <a@lapets.io>
2019-12-29 03:22:46 -0500

    Follow conventions for variable names.

--- server.js
+++ server.js
@@ -116,13 +116,13 @@ io.on('connection', function (socket) {
 
   socket.on('oblv', function(params) {
     console.log('oblv', params);
-    const msg_id = params.msg_id;
+    const msgId = params.msgId;
     const length = params.length;
 
     var r0, r1;
-    if (cache[msg_id] === undefined || cache[msg_id].unused) {
-      if (cache[msg_id] === undefined) {
-        cache[msg_id] = {unused: true};  // or with just {}
+    if (cache[msgId] === undefined || cache[msgId].unused) {
+      if (cache[msgId] === undefined) {
+        cache[msgId] = {unused: true};  // or with just {}
       }
       r0 = [];
       r1 = [];
@@ -130,22 +130,22 @@ io.on('connection', function (socket) {
         r0[i] = sodium.randombytes_uniform(256);
         r1[i] = sodium.randombytes_uniform(256);
       }
-      cache[msg_id].r0 = r0;
-      cache[msg_id].r1 = r1;
-      cache[msg_id].unused = false;
+      cache[msgId].r0 = r0;
+      cache[msgId].r1 = r1;
+      cache[msgId].unused = false;
     } else {
-      r0 = cache[msg_id].r0;
-      r1 = cache[msg_id].r1;
-      cache[msg_id] = {unused: true};  // clear cache
+      r0 = cache[msgId].r0;
+      r1 = cache[msgId].r1;
+      cache[msgId] = {unused: true};  // clear cache
     }
 
     if (socket.id === party.garbler) {
-      socket.emit('oblv'+msg_id, JSON.stringify([r0, r1]));
+      socket.emit('oblv'+msgId, JSON.stringify([r0, r1]));
     }
 
     if (socket.id === party.evaluator) {
       const d = sodium.randombytes_uniform(2);
-      socket.emit('oblv'+msg_id, JSON.stringify([d, d ? r1 : r0]));
+      socket.emit('oblv'+msgId, JSON.stringify([d, d ? r1 : r0]));
     }
   });
 });
